8c51dd4b9a6ce1f1ce11c1ba02869115aebe46a5
Frozenlock <frozenlock@gmail.com>
2012-11-27 12:39:04 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject lein-getdown "0.1.0-SNAPSHOT"
+  :description "Lein plugin to wrap the Getdown java application.
+Provides an easy way to make auto-update clojure applications."
+  :url "https://github.com/Frozenlock/lein-getdown"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true)
---
+++ src/leiningen/getdown.clj
@@ -0,0 +1,6 @@
+(ns leiningen.getdown)
+
+(defn getdown
+  "I don't do a lot."
+  [project & args]
+  (println "Hi!"))
